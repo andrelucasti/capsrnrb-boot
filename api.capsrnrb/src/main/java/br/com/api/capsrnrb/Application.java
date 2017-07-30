fad711302edc01b9b82e5359f257be90630613d7
Andre <Andre@andre-gamer>
2017-07-30 19:55:01 -0300

    Enable Spring Boot Servlet

--- api.capsrnrb/src/main/java/br/com/api/capsrnrb/Application.java
+++ api.capsrnrb/src/main/java/br/com/api/capsrnrb/Application.java
@@ -1,17 +1,15 @@
 package br.com.api.capsrnrb;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		System.out.println("Hue");
-		
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 	
-	
-
 }
