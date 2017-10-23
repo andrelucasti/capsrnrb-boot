17caeda756f98eb48a923a8962594a61108f3128
andrelucas <andrelucastic@gmail.com>
2017-10-22 22:10:50 -0300

    ALTER SCRIPT

--- api.capsrnrb/src/main/java/br/com/api/capsrnrb/models/Root.java
+++ api.capsrnrb/src/main/java/br/com/api/capsrnrb/models/Root.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -29,7 +30,7 @@ public class Root implements Serializable{
 	private String password;
 	
 
-	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE, })
+	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
 	private Token token;
 	
 	
--- api.capsrnrb/src/main/resources/db/migration/V04__create_table_address.sql
+++ api.capsrnrb/src/main/resources/db/migration/V04__create_table_address.sql
@@ -1,40 +1,40 @@
 CREATE table IF NOT EXISTS address (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-street VARCHAR(100), 
-zipcode VARCHAR(50),
-district VARCHAR(50),
-complement varchar(100),
-id_city INT NOT NULL, 
-FOREIGN KEY (id_city) REFERENCES city(id)
+  id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  complement varchar(255) DEFAULT NULL,
+  district varchar(200) DEFAULT NULL,
+  number int DEFAULT NULL,
+  street varchar(255) DEFAULT NULL,
+  zip_code varchar(255) DEFAULT NULL,
+  city_id int(11) DEFAULT NULL,
+FOREIGN KEY (city_id) REFERENCES city(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
 CREATE table IF NOT EXISTS user (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(200), 
-mother_name VARCHAR(200),
-nu_prontuario INTEGER,
-nu_cart_sus varchar(100),
-file_local varchar(10),
-drug_type varchar(50),
-phone varchar(50),
-date_of_birth date,
-id_addresss INT NOT NULL, 
-FOREIGN KEY (id_addresss) REFERENCES address(id)
+  id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  date_of_birth datetime DEFAULT NULL,
+  drug_type varchar(255) DEFAULT NULL,
+  file_local varchar(255) DEFAULT NULL,
+  mother_name varchar(255) DEFAULT NULL,
+  name varchar(255) DEFAULT NULL,
+  nu_cart_sus varchar(255) DEFAULT NULL,
+  nu_prontuario bigint(20) DEFAULT NULL,
+  phone varchar(255) DEFAULT NULL,
+  address_id bigint, 
+FOREIGN KEY (address_id) REFERENCES address(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE table IF NOT EXISTS screeningdate (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
 screening_date date not null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE table IF NOT EXISTS user_screeningdate (
-id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-user_id INTEGER NOT NULL,
-screeningdate_id INTEGER NOT NULL
+user_id bigint NOT NULL,
+screeningdate_id bigint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
